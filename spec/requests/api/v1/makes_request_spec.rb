ed9d9d8d3f8ee34df8fb4b9191c8091c9ce46cbe
Padraic O'Neill <pconeill@gmail.com [core]>
2018-03-01 10:24:29 -0700

    add makes request spec examples

--- spec/requests/api/v1/makes_request_spec.rb
+++ spec/requests/api/v1/makes_request_spec.rb
@@ -2,22 +2,43 @@ require 'rails_helper'
 
 describe "Makes API" do
 
-  it "can return a list of all vehicle makes" do
-    car = create(:make)
+  it "sends a list of all vehicle makes" do
+    cars = create_list(:make, 10)
 
-    expect(car).to be_a(Make)
+    get '/api/v1/makes'
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+
+    expect(json['makes'].length).to eq(10)
   end
 
-  xit "can create a new vehicle make" do
+  it "sends a list of all vehicle makes that includes attributes" do
+    cars = create_list(:make, 10)
+    first_car_name = cars[0].name
+    last_car_name = cars[9].name
 
+    get '/api/v1/makes'
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+
+    expect(json['makes'][0].name).to eq(first_car_name)
+    expect(json['makes'][9].name).to eq(last_car_name)
   end
 
-  xit "can update an existing vehicle make" do
-
-  end
-
-  xit "can delete an existing vehicle make" do
-
-  end
+  # xit "can create a new vehicle make" do
+  #
+  # end
+  #
+  # xit "can update an existing vehicle make" do
+  #
+  # end
+  #
+  # xit "can delete an existing vehicle make" do
+  #
+  # end
 
 end
