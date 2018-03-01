7778046f12f24e732e03bab9ff511243237b46fd
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-28 19:23:40 -0700

    confirm rspec, factorybot, and faker are working

--- spec/requests/api/v1/makes_request_spec.rb
+++ spec/requests/api/v1/makes_request_spec.rb
@@ -5,7 +5,7 @@ describe "Makes API" do
   it "can return a list of all vehicle makes" do
     car = create(:make)
 
-    expect(car).to_not be_empty
+    expect(car).to be_a(Make)
   end
 
   xit "can create a new vehicle make" do
