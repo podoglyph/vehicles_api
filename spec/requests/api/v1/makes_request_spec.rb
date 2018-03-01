f332caecf641bb83a7a673bcb52aeb339f57b2e0
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-28 18:13:37 -0700

    create structure for vehicle make spec including CRUD

---
+++ spec/requests/api/v1/makes_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Makes API" do
+
+  it "can return a list of all vehicle makes" do
+
+  end
+
+  it "can create a new vehicle make" do
+
+  end
+
+  it "can update an existing vehicle make" do
+
+  end
+
+  it "can delete an existing vehicle make" do
+
+  end
+  
+end
